848c866b9b3f338bcf765bbd18fb71c3ee24bcba
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-06 10:57:24 -0400

    Added prompts to the inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
-
+let inquirer = require("inquirer");
+let fs = require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please input a title:",
+        default: "title"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please input a description:",
+        default: "description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please input installation instructions:",
+        default: "install"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please input usage information:",
+        default: "usage"
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please input contribution guidelines:",
+        default: "contribute"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please input test instructions:",
+        default: "test"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please input a contact email:",
+        default: "email"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please select a license:",
+        choices: ["MIT", "IBM", "ISC"]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please input your GitHub username:",
+        default: "github"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(response => {
+        const {title, description, installation, usage, contribution, test, email, license, github} = response;
+        console.log(title, description, installation, usage, contribution, test, email, license, github);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    ISC: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    IBM: "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
